2981e78fadc5d57d759eb0ae7839522f4124dcb3
refrigerated-c <shivangi28mishra@gmail.com>
2025-08-20 15:31:45 +0000

    More

---
+++ 01 js basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 1234
+let accountEmail ="shivangi@goole.com"
+var accountPassword ="1444"
+accountCity="Jaipur"
+
+// accountId = 2
+
+accountEmail ="dhdh@google.com"
+accountPassword ="3456"
+accountCity ="Mumbai"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
+
