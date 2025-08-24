c8ffd2e30fd79d92831b13cc9bfc81bfb94a5f70
refrigerated-c <shivangi28mishra@gmail.com>
2025-08-24 15:34:32 +0000

    03 conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+ let score = false
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber); 
+
+//"33" =>33
+//"33abx" => Nan (Not an Number), but value's type is number
+// true =>1 ; false => 0 
+//null => 0; undefined => Nan
+
+let isLoggedIn = "shiv"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+//1 => true; 0 => false
+//"" => false
+// "shiva" => true
+
+let someNumber = 77
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
