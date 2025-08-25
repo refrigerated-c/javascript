d9dd090b502e51c962d8cc12dfec455654820524
refrigerated-c <shivangi28mishra@gmail.com>
2025-08-25 14:58:02 +0000

    operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -24,5 +24,40 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 77
 
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+//************** Operations *************************//
+
+let value = 3
+let negvalue = -value
+//console.log(negvalue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**4); //power
+// console.log(2/3);
+// console.log(2%6);
+
+let str1 = "hello"
+let str2 = " Shiv"
+
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1"+ 2)
+// console.log(1+ "2")
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
+console.log("5" - 2);
+
+
+// console.log(3 + 4 * 5 % 3) // no  never do this
+
+// console.log(+true)
+// console.log(+"")
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
