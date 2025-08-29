93aea1d9b405ca49872f8600174aa114d2db9a1a
refrigerated-c <shivangi28mishra@gmail.com>
2025-08-29 15:18:59 +0000

    stack & heap

--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -27,3 +27,32 @@ console.log(typeof heros)
 console.log(typeof myObj)
 
 // Refrence / Non-Primitive ka datatype - object ki aayega or function ka - object function
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//Stack (Primitive), Heap (Non-Primitive)
+//      |                  |
+//      v                  v  
+// copy of declared ,   reference 
+//     variable
+
+let myYoutubename = "shivangimishra"
+
+let anothername = myYoutubename    //same name of 1st var
+anothername = "spmExplorer"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne  // here we got the reference of original not copy
+
+userTwo.email = "shiv@google.com"  //changing here means changing the original
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
