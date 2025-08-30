a581934b3bbd5c062e1b5798300ceb649b0cb319
refrigerated-c <shivangi28mishra@gmail.com>
2025-08-30 15:01:32 +0000

    strings 1

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,17 @@
+const name = "shivangi"
+const repoCount = 50
+
+// console.log(name + repoCount + " Value");  //old way
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);  //string interpolation way
+
+const gameName = new String('shivangipm')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase())
+console.log(gameName.charAt('2'))
+console.log(gameName.indexOf('v'))
+
