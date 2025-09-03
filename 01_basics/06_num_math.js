60454e930ffdc0f7264c597e5e66a574b6214a5f
refrigerated-c <shivangi28mishra@gmail.com>
2025-09-03 04:06:00 +0000

    06-num

---
+++ 01_basics/06_num_math.js
@@ -0,0 +1,7 @@
+const score = 400
+console.log(score)//JS auto detects it as a number
+
+const balance = new Number(100) //Using number function to explicitly define number in JS
+console.log(balance)
+
+//Note : Number has comparativly less prototypes properties and methods than string
