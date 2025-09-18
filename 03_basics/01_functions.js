a12ea69d4a1743f2b2c9219f33589cafe50aa34f
refrigerated-c <shivangi28mishra@gmail.com>
2025-09-18 14:41:12 +0000

    functions1

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,12 @@
+function sayMyName (){
+    console.log("S");
+    console.log("H");
+    console.log("I");
+    console.log("V");
+    console.log("A");
+    console.log("N");
+    console.log("G");
+    console.log("I");
+    }
+
+//sayMyName()
