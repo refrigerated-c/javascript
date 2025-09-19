faff20236619591f6654f4e53ab0630f83607c51
refrigerated-c <shivangi28mishra@gmail.com>
2025-09-19 03:50:27 +0000

    functions2

---
+++ 03_basics/01_1_functions.js
@@ -0,0 +1,10 @@
+ function calculateCartPrice1(num1){
+     return num1
+ }
+ console.log(calculateCartPrice1(200, 400, 300));
+
+//use of rest operator
+function calculateCartPrice2(...num1){
+    return num1
+}
+console.log(calculateCartPrice2(200, 400, 300));
--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -10,3 +10,62 @@ function sayMyName (){
     }
 
 //sayMyName()
+
+// function addTwoNumbers(num1, num2){
+//     console.log(num1 + num2);
+// }
+// addTwoNumbers(3,4)
+
+// function addTwoNumbers(num1, num2){
+//    console.log(num1 + num2);
+// }
+
+// const result = addTwoNumbers(3,4) //for storing a value in a const
+
+// console.log("Results: ", result); // Result: undefined coz using log
+
+
+//after using return so other function will run 
+//for example -
+// function addTwoNumbers(num1, num2){
+//    let result = num1 + num2
+//    return result
+// console.log(Hi); // its showing unreachable right here
+
+// }
+function addTwoNumbers(num1, num2){
+//    let result = num1 + num2
+//    return result
+     return num1 + num2  //one more try
+}
+const result = addTwoNumbers(3,4) //for storing a value in a const
+
+//console.log("Results: ", result); //Result : 7 -> coz used return
+
+// function loginUserName (username){
+//     return `${username} just logged in`
+// }
+//console.log(loginUserName("Shiva")); // return only returns when its stored in or log to print it
+
+//what if no argument passed 
+// console.log(loginUserName()); //undefined
+//for this use of if else
+
+// function loginUserName (username){
+//     if ((username === undefined)) {             //one more way for parameters (!username)
+//         console.log("Please enter username");
+//         return
+//     }
+//     return `${username} just logged in`   //coz we use return above in if, this return will not work
+// }    
+// console.log(loginUserName());
+
+function loginUserName(username= "employee")//default username 
+{
+    if (!username) {             
+        console.log("Please enter username");
+        return
+    }
+    return `${username} just logged in`   
+}
+console.log(loginUserName("Shivangi")); // & this argument will overwrite "employee"
