bf244487dac78b48658bd2824c03546332eef880
refrigerated-c <shivangi28mishra@gmail.com>
2025-10-08 18:40:36 +0000

    scopes

--- 03_basics/01_1_functions.js
+++ 03_basics/01_1_functions.js
@@ -1,7 +1,7 @@
  function calculateCartPrice1(num1){
      return num1
  }
- console.log(calculateCartPrice1(200, 400, 300)); // 200
+ //console.log(calculateCartPrice1(200, 400, 300)); // 200
 
 //use of rest operator
 function calculateCartPrice2(...num1){
@@ -9,33 +9,33 @@ function calculateCartPrice2(...num1){
 }
 //console.log(calculateCartPrice2(200, 400, 300)); //[ 200, 400, 300 ]
 
-function calculateCartPrice2(val1, val2, ...num1){
-    return num1
-}
-//console.log(calculateCartPrice2(200, 400, 300, 500, 700,800)); //200 in val1, 400 in val2, and rest in num & printed
+// function calculateCartPrice2(val1, val2, ...num1){
+//   return num1
+// }
+// console.log(calculateCartPrice2(200, 400, 300, 500, 700,800)); //200 in val1, 400 in val2, and rest in num & printed
 
-const user = {
-    user: "Shiv",
-    price: 999
-}
+ const user = {
+     user: "Shiv",
+     price: 999
+ }
 function handleObject(anyobject){
     console.log(`Username is ${anyobject.user} and price is ${anyobject.price}`);
 
 }
-//handleObject(user)
+handleObject(user)
 handleObject({          //another way to use object
     user: "sam",
     price: 299
 })
 
-//For Arrays
+For Arrays
 
-const myNewArray = [200, 300, 400,500]
+ const myNewArray = [200, 300, 400,500]
 
-function returnSecondValue(getArray){
-    return getArray[1]
-}
-//console.log(returnSecondValue(myNewArray));
+ function returnSecondValue(getArray){
+     return getArray[1]
+ }
+console.log(returnSecondValue(myNewArray));
 console.log(returnSecondValue([1000,5000,9000,400])); //another way for passing array
 //! kmwqkmsqdmjd
 //! THis is a comment
