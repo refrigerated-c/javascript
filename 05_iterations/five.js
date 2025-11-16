b99d05f5e7a84b2d3c6e588649d512d723a20bb2
refrigerated-c <shivangi28mishra@gmail.com>
2025-11-16 19:00:52 +0000

    forEach

---
+++ 05_iterations/five.js
@@ -0,0 +1,54 @@
+const coding = ["js", "py", "ruby", "Kerim"]
+
+// coding.forEach( function(item){             //no need to write name as it understands that it's a callback funct for which arr
+//     console.log(item);
+    
+// })
+
+//in arrow function
+
+coding.forEach( (val) => {
+ //console.log(val);
+ 
+})
+
+// normal function bahar bhi likh ke call kr skte hai
+
+function printMe(item) {
+    //console.log(item);
+    
+}
+
+// coding.forEach(printMe) //just reference it not execute it with ()
+
+//foreach not only use item or val as param but also others too
+
+// coding.forEach((item, index, arr)=>{
+//     console.log(item, index, arr);
+    
+// }) //js 0 [ 'js', 'py', 'ruby', 'Kerim' ]
+// py 1 [ 'js', 'py', 'ruby', 'Kerim' ]
+// ruby 2 [ 'js', 'py', 'ruby', 'Kerim' ]
+// Kerim 3 [ 'js', 'py', 'ruby', 'Kerim' ]
+
+//Now as we seen that objects in array in three.js , so now let's put loop in it
+
+const myChar = [
+    {
+        characterName: "Kerim",
+        showName: "Fatmagul"
+    },
+    {
+        characterName: "Shahan",
+        showName: "tatli ask"
+    },
+    {
+        characterName: "Gulten",
+        showName: "Kuzey Guney"
+    }
+]
+
+myChar.forEach((item)=>{
+    console.log(item.showName);
+    
+})
---
+++ 05_iterations/four.js
@@ -0,0 +1,37 @@
+//iteration in objects
+//for in
+
+const myObjct = {
+    js: 'javascript',
+    cpp: 'C++',
+    rb: 'ruby',
+    swift: "swift by apple"
+
+}
+for (const key in myObjct) {
+    //console.log(key); //only for keys
+    //console.log(myObjct[key]); //for values of key
+    //console.log(`${key} shortcut is for ${myObjct[key]}`); //for both
+    
+
+}
+//in array
+const programming = ["py", "js", "rb", "java", "cpp"]
+
+for (const key in programming) {
+    //console.log(key);
+    // result will show here in 0 to 4 (specifcaly no. of array)
+    //console.log(programming[key]);
+}
+
+//for in in maps
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('FR', "France")
+
+for (const key in map) {
+   console.log(key);
+   
+    
+}
