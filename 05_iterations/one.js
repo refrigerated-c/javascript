94caac8f0a9ac821cd0cb1aba89c7abf0fc92264
refrigerated-c <shivangi28mishra@gmail.com>
2025-11-27 07:51:46 +0000

    check

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -1,58 +1,58 @@
 //for
                 //  this pooooooooortion (is known as condition check)
-// for (let i = 0; i <= 10; i++) {
-//     const element = i;
-//     console.log(element);
-// }
+//  for (let i = 0; i <= 10; i++) {
+//      const element = i;
+//      console.log(element);
+//  }
 
-// for (let i = 0; i <= 10; i++) {
-//     const element = i;
-//     if (element == 5){
-//         console.log("5 is best number")
+//  for (let i = 0; i <= 10; i++) {
+//      const element = i;
+//      if (element == 5){
+//         //console.log("5 is best number")
 //     }
-//     console.log(element);
-// }
+//      //console.log(element);
+//  }
 
-for (let i = 1; i <= 10; i++) {
-    //console.log(`Outer loop value: ${i}`);
-    for (let j = 0; j <=10 ; j++) {
-        //console.log(`Inner Loop value ${j} and outer loop value ${i}`)
-        //console.log(i + `x` + j + `=` + i*j); // printing tables
+// for (let i = 1; i <= 10; i++) {
+//     //console.log(`Outer loop value: ${i}`);
+//     for (let j = 0; j <=10 ; j++) {
+//         console.log(`Inner Loop value ${j} and outer loop value ${i}`)
+//         console.log(i + `x` + j + `=` + i*j); // printing tables
         
-    }
-}
+//     }
+// }
 
 //Array
 let myArray = ["iron-man", "Spiderman", "blackwidow"]
 //console.log(myArray.length);
 
-for (let index = 0; index < myArray.length; index++) {
-    const element = myArray[index];
-    //console.log(element);
+// for (let index = 0; index < myArray.length; index++) {
+//     const element = myArray[index];
+//     console.log(element);
     
-}
+// }
 //1st- we wrote index (less than) < myArray.length not <= it will result undefined coz lenght of array is 3 but actual how 0, 1, 2 and there is in actual no 3rd array if starts from 0.
 
 //2nd - index "++" is imp else the 1st array will forever run 
 
 //Break
 
- for (let index = 1; index <= 20; index++) {
-     if (index == 5) {
-         //console.log(`Detected 5`);
-         break
-     }
-     //console.log(`Value of i is ${index}`);
+//  for (let index = 1; index <= 20; index++) {
+//      if (index == 5) {
+//          console.log(`Detected 5`);
+//          break
+//      }
+//      console.log(`Value of i is ${index}`);
     
     
- }
+//  }
 //Continue
-for (let index = 1; index <= 20; index++) {
-    if (index == 5) {
-        console.log(`Detected 5`);
-        continue
-    }
-    console.log(`Value of i is ${index}`);
+// for (let index = 1; index <= 20; index++) {
+//     if (index == 5) {
+//         console.log(`Detected 5`);
+//         continue
+//     }
+//     console.log(`Value of i is ${index}`);
     
     
-}
+// }
--- 05_iterations/two.js
+++ 05_iterations/two.js
@@ -1,14 +1,14 @@
 let index = 0
-// while (index <= 10) {
-//     console.log(`Value of index is ${index}`);
-//     index = index + 2
-// }
+ while (index <= 10) {
+     //console.log(`Value of index is ${index}`);
+     index = index + 2
+ }
 
 let myArray = ["Simi", "Maheen", "Bahar"]
 let arr = 0
 
 while (arr < myArray.length) {
-    //console.log(`Artist of Tatli Ask are ${myArray[arr]}`);  //implicity write arr so that array's index run according to arr value
+    //console.log(`Artist of Tatli Ask is ${myArray[arr]}`);  //implicity write arr so that array's index run according to arr value
     arr = arr + 1
 
 }
@@ -16,11 +16,11 @@ while (arr < myArray.length) {
 //Do While loop
 let score = 0
 do {
-    console.log(`Score is ${score}`);
+    //console.log(`Score is ${score}`);
     score++
 
 } while (score <= 10);
-//it will not only till 10 but also print above  10 numbers coz here condition is checked after loop is printed
+//here above 10 number can also be printed coz here condition is checked after loop is printed
 
 // let score = 12
 // do {
