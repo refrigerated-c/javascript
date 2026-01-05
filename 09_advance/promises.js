710c2c017d186496543170429b0c0feea640b594
refrigerated-c <shivangi28mishra@gmail.com>
2026-01-05 12:39:40 +0000

    promise03

--- 09_advance/promises.js
+++ 09_advance/promises.js
@@ -66,6 +66,63 @@ promiseFour
 }).catch((err)=>{
     console.log(err);
     
+}).finally(()=> {
+    console.log("The promise is either resolved or rejected");
+    
 })
 
+const promiseFive = new Promise (function (resolve, reject){
+    setTimeout(()=>{
+        let error = false;
+        if (!error){
+            resolve({username: "Tokyo", password: "123"})
+        } else{
+            reject('ERROR: JS went wrong')
+        }
+    },1000)
+})
 
+//instead of .then and .catch
+async function consumePromiseFive(){
+        const response = await promiseFive;
+        console.log(response);
+}       // result if error value is true -> UnhandledPromiseRejection: This error originated either by throwing inside of an async function without a catch block, or by rejecting a promise which was not handled with .catch(). The promise rejected with the reason "ERROR: JS went wrong".
+
+//So use of try and catch
+
+
+//  async function consumePromiseFive(){
+//     try {
+//         const response = await promiseFive;
+//         console.log(response);
+        
+//     } catch (error) {
+//         console.log(error);
+        
+//     }
+
+//  }
+//  consumePromiseFive()
+
+// async function getAllUsers() {
+//     try {
+//         const response = await fetch ('https://jsonplaceholder.typicode.com/users')
+//     const data = await response.json() //this await is imp as it takes time to fetch data
+//     console.log(data);
+//     } catch (error) {
+//         console.log('Error:', error);
+        
+//     }
+    
+    
+// }  
+// getAllUsers() 
+ 
+fetch ('https://jsonplaceholder.typicode.com/users')
+.then ((response)=>{
+    return response.json()
+})
+.then((data)=>{
+    console.log(data);
+    
+}).catch((err)=> console.log(err))
